341235350dbbed78e421650ccc1ffc09632e6fdb
ALOKYAD <81628249+ALOKYAD@users.noreply.github.com>
2022-12-12 16:23:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.title('My Parents New Healthy Diner')
 
- print("Hello World")
-
-
 st.write("Breakefast Menu:")
  st.write("Omega 3 & Blueberry Oatmeal:")
  st.write("Kale ,Spinanch & Rocket smoothie:")
