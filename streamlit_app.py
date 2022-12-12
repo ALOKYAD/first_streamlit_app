3f50e731c4b5186e4c182b5a557430bc85c35160
ALOKYAD <81628249+ALOKYAD@users.noreply.github.com>
2022-12-12 15:52:25 +0530

    Update streamlit_app.py
    updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 
 streamlit.title('Breakefast Menu')
- Omega 3 & Blueberry Oatmeal
- Kale ,Spinanch & Rocket smoothie
-Hard-Boiled Free-range Egg
+ streamlit('Omega 3 & Blueberry Oatmeal')
+ streamlit('Kale ,Spinanch & Rocket smoothie')
+('Hard-Boiled Free-range Egg')
