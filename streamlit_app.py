aa9df4cb8191e0d141d8db0aee4eb3757e79bb8b
ALOKYAD <81628249+ALOKYAD@users.noreply.github.com>
2022-12-12 16:21:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('My Parents New Healthy Diner')
 
 
-st.write("Breakefast Menu:")
+print("Breakefast Menu:")
  st.write("Omega 3 & Blueberry Oatmeal:")
  st.write("Kale ,Spinanch & Rocket smoothie:")
 st.write("Hard-Boiled Free-range Egg:")
