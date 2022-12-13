28d052cfa58179574dfaf6341118d3751ca8e18d
ALOKYAD <81628249+ALOKYAD@users.noreply.github.com>
2022-12-13 10:43:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-
-streamlit.header('Breakefast Menu')
  streamlit.text('Omega 3 & Blueberry Oatmeal')
  streamlit.text('Kale ,Spinanch & Rocket smoothie')
 streamlit.text('Hard-Boiled Free-range Egg')
