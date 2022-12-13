e98148eb5472845d6a12d7a5cb31ba31fd062556
ALOKYAD <81628249+ALOKYAD@users.noreply.github.com>
2022-12-13 11:13:00 +0530

    Update streamlit_app.py
    import pandas and updated file

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,6 @@ streamlit.text('🥗 🥪 Kale ,Spinanch & Rocket smoothie')
 streamlit.text('🥘 Hard-Boiled Free-range Egg')
 
 
+import pandas as pd
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
